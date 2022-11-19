310863b9fa09384247d93cc9c83985f2bac44614
RInRInPark <surinp8019@naver.com>
2022-11-19 23:33:41 +0900

    conflict

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-// src/index.ts
 import express, { NextFunction, Request, Response } from "express";
 
 const app = express(); // express 객체 받아옴
@@ -6,17 +5,17 @@ const PORT = 3000; // 사용할 port를 3000번으로 설정
 
 app.use(express.json()); // express 에서 request body를 json 으로 받아오겠다.
 
-//app.use("/api", require("./api")); // use -> 모든 요청
+app.use("/api", require("./api")); // use -> 모든 요청
 // localhost:8000/api -> api 폴더
 // localhost:8000/api/user -> user.ts
 
 //* HTTP method - GET
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
-    res.send("마! 이게 서버다!");
+  res.send("마! 이게 서버다!");
 });
 
 app.listen(PORT, () => {
-    console.log(`
+  console.log(`
         #############################################
             🛡️ Server listening on port: ${PORT} 🛡️
         #############################################
