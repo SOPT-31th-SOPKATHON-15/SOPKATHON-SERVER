91cc821628a1d107ad3c7c969f6b70f0b81334f6
dahyunkim <dahyun_ee@naver.com>
2022-11-20 03:54:22 +0900

    [Fix] 라우터 중복

--- src/controller/ProductController.ts
+++ src/controller/ProductController.ts
@@ -11,7 +11,6 @@ const getProductList = async (req: Request, res: Response) => {
 
   const data = await productService.getProducts(String(year), String(month));
   if(!data) return res.status(sc.NOT_FOUND).send(fail(sc.NOT_FOUND, rm.NULL_VALUE));
-  console.log(data);
   res.status(sc.OK).send(success(sc.OK, rm.GET_POP_UP_SUCCESS, data));
 };
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ import productRouter from "./ProductRouter";
 
 const router: Router = Router();
 
-router.use("/products", productRouter);
+router.use("/product-list", productRouter);
 router.use("/products", popUpRouter);
 
 export default router;
